de0e724be189b2b57bd1feade46d381edfe187be
Ze Gan <ganze718@gmail.com>
2019-12-10 00:00:24 +0800

    Add nccl script

--- PyTorch/LanguageModeling/BERT/run_all.sh
+++ PyTorch/LanguageModeling/BERT/run_all.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-log_dir="logs"
+log_dir="bert-logs"
+gpu_total_count=$(ls -l /dev | grep -E "nvidia[0-9]" | wc -l)
+
 mkdir ${log_dir} -p
 
 for batch_size in 1 2 3 4 5 6
 do
-    for gpu_count in $(seq 1 `ls -l /dev | grep -E "nvidia[0-9]" | wc -l`)
+    for gpu_count in 1 2 4 8 16
     do
-        echo "gpu_count $gpu_count batch_size $batch_size"
-        bash scripts/run_pretraining.pyt_hvd.sh $batch_size "6e-3" $gpu_count | tee ${log_dir}/gpu_count_${gpu_count}_batch_size_${batch_size}.txt
-	sleep 5
+        if [ "${gpu_count}" -le "${gpu_total_count}" ]
+        then
+            echo "gpu_count $gpu_count batch_size $batch_size"
+            bash scripts/run_pretraining.pyt_hvd.sh $batch_size "6e-3" $gpu_count | tee ${log_dir}/gpu_count_${gpu_count}_batch_size_${batch_size}.txt
+	        sleep 5
+        fi
     done
 done
---
+++ PyTorch/LanguageModeling/BERT/run_nccl_profiling.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+log_dir="nccl-logs"
+nccl_test_dir="nccl-tests"
+gpu_total_count=$(ls -l /dev | grep -E "nvidia[0-9]" | wc -l)
+
+mkdir -p ${log_dir}
+
+if [ ! -d ${nccl_test_dir} ]
+then
+    git clone https://github.com/NVIDIA/nccl-tests.git ${nccl_test_dir}
+fi
+
+cd ${nccl_test_dir}
+make
+for op in "all_gather" "all_reduce" "broadcast" "reduce" "reduce_scatter"
+do
+    for gpu_count in 1 2 4 8 16
+    do
+        if [ "${gpu_count}" -le "${gpu_total_count}" ]
+        then
+            echo "gpu_count ${op} op ${op}"
+            build/${op}_perf -b 8M -e 1G -f 2 -g ${gpu_count} | tee ${log_dir}/gpu_count_${gpu_count}_op_${op}.txt
+        fi
+    done
+done
