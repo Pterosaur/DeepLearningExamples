0868c786c90e9929d609dad14aca8e1b53040e5a
Ze Gan <ganze718@gmail.com>
2019-12-09 15:38:20 +0800

    Change batch script to dynamically detect gpu count

--- PyTorch/LanguageModeling/BERT/run_all.sh
+++ PyTorch/LanguageModeling/BERT/run_all.sh
@@ -5,7 +5,7 @@ mkdir ${log_dir} -p
 
 for batch_size in 1 2 3 4 5 6
 do
-    for gpu_count in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+    for gpu_count in $(seq 1 `ls -l /dev | grep -E "nvidia[0-9]" | wc -l`)
     do
         echo "gpu_count $gpu_count batch_size $batch_size"
         bash scripts/run_pretraining.pyt_hvd.sh $batch_size "6e-3" $gpu_count | tee ${log_dir}/gpu_count_${gpu_count}_batch_size_${batch_size}.txt
